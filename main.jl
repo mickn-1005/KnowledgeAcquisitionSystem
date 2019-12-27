4623bf66e6bc3ce04eca8d198b21930de86a3275
mickn <wataru1005mikkun@gmail.com>
2019-12-27 17:54:45 +0900

    tlmデータの数値カテゴリ化

--- main.jl
+++ main.jl
@@ -1,1 +1,32 @@
-using Plots, DataFrames
+using Plots, DataFrames, CSV, ScikitLearn
+
+filepath = "data/dat41/191121_HK_real_svtlm.csv"
+
+# CSV.validate(filepath)
+
+tlm = CSV.read(filepath)
+tlmnm = names(tlm)
+
+# k = eltypes(tlm)
+# for types in k
+#     if Missing == types     # 欠損値が含まれないか確認
+#         println("hoge")
+#     end
+# end
+# unique(k)
+
+for i in tlmnm
+    println(i)
+end
+
+tlm[!,tlmnm[1]]
+
+mode_category = unique(tlm[!, tlmnm[4]])
+
+for nm in tlmnm
+    if eltype(tlm[nm])==String
+        catlist = unique(tlm[!, nm])
+        tlm[!, nm] = [findfirst(==(x), catlist) - 1 for x in tlm[!, nm]]    # ラベルを数値に変換．
+    end
+end
+
