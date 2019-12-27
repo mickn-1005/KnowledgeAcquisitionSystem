249b311a77dba210128e01c051fb3cbd6b05d312
mickn <wataru1005mikkun@gmail.com>
2019-12-27 23:55:56 +0900

    コード補正

--- main.jl
+++ main.jl
@@ -7,7 +7,7 @@ filepath = "data/dat41/191121_HK_real_svtlm.csv"
 tlm = CSV.read(filepath)
 tlmnm = names(tlm)
 
-# k = eltypes(tlm)
+k = unique(eltype.(eachcol(tlm)))
 # for types in k
 #     if Missing == types     # 欠損値が含まれないか確認
 #         println("hoge")
@@ -15,18 +15,17 @@ tlmnm = names(tlm)
 # end
 # unique(k)
 
-for i in tlmnm
-    println(i)
-end
-
 tlm[!,tlmnm[1]]
 
-mode_category = unique(tlm[!, tlmnm[4]])
+mode_category = tlm[!, tlmnm[5]]
 
 for nm in tlmnm
-    if eltype(tlm[nm])==String
+    if eltype(tlm[!, nm])==String
         catlist = unique(tlm[!, nm])
         tlm[!, nm] = [findfirst(==(x), catlist) - 1 for x in tlm[!, nm]]    # ラベルを数値に変換．
     end
 end
 
+a = [1,2,3]
+
+k = tlm[!, a]
