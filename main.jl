cd65130ddd0402485609ac26aef08411d4524f9d
mickn-1005 <wataru1005mikkun@gmail.com>
2019-12-29 19:44:10 +0900

    ライブラリを使用してフィージビリティを確認

--- main.jl
+++ main.jl
@@ -1,15 +1,19 @@
-using Plots, DataFrames, CSV#, ScikitLearn
+using Plots, DataFrames, CSV
+using TSne
+# using ScikitLearn
 
-filepath = "data/dat41/191121_HK_real_svtlm.csv"
+filepath = "data/dat41/191122_HK_real_svtlm.csv"
 
 tlm = CSV.read(filepath)
+tlm = dropmissing(tlm)      # 欠損地を含む行をデータから削除
 tlmnm = names(tlm)
+
 
 k = unique(eltype.(eachcol(tlm)))
 
 tlm[!,tlmnm[1]]
 
-mode_category = tlm[!, tlmnm[5]]
+mode_category = tlm[!, tlmnm[4]]
 
 for nm in tlmnm
     if eltype(tlm[!, nm])==String
@@ -18,6 +22,18 @@ for nm in tlmnm
     end
 end
 
-a = [1,2,3]
+"""
+説明変数のみを格納したDataFrameの作成
+"""
+express = Matrix(tlm[7:end])
+pred = tsne(express)
 
-k = tlm[!, a]
+length(pred)
+
+scatter(pred[:,1], pred[:,2])
+
+scatter(pred[findall(==(unique(mode_category)[1]), tlm[!, 4]), 1], pred[findall(==(unique(mode_category)[1]), tlm[!, 4]),2])
+scatter!(pred[findall(==(unique(mode_category)[2]), tlm[!, 4]), 1], pred[findall(==(unique(mode_category)[2]), tlm[!, 4]),2])
+scatter!(pred[findall(==(unique(mode_category)[3]), tlm[!, 4]), 1], pred[findall(==(unique(mode_category)[3]), tlm[!, 4]),2])
+
+
