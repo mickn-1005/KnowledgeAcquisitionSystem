ca9364dd0c2b0ed907e90add56b508e7581e492c
mickn-1005 <wataru1005mikkun@gmail.com>
2019-12-29 22:43:13 +0900

    データの正規化

--- main.jl
+++ main.jl
@@ -15,25 +15,38 @@ tlm[!,tlmnm[1]]
 
 mode_category = tlm[!, tlmnm[4]]
 
-for nm in tlmnm
-    if eltype(tlm[!, nm])==String
-        catlist = unique(tlm[!, nm])
-        tlm[!, nm] = [findfirst(==(x), catlist) - 1 for x in tlm[!, nm]]    # ラベルを数値に変換．
+function enum_modeid(tlm, tlmnm)
+    for nm in tlmnm
+        if eltype(tlm[!, nm])==String
+            catlist = unique(tlm[!, nm])
+            tlm[!, nm] = [findfirst(==(x), catlist) - 1 for x in tlm[!, nm]]    # ラベルを数値に変換．
+        end
     end
+    return tlm
+end
+tlm = enum_modeid(tlm, tlmnm)
+numerized_mc = tlm[!, tlmnm[4]]
+
+function df_scale(df)
+    d = describe(df, :mean, :std)
+    for i in length(size(df)[2])
+        df[i] = (df[i] .- d[i,3]) / d[i,2]
+    end
+    return df
 end
 
-numerized_mc = tlm[!, tlmnm[4]]
+tlm = df_scale(tlm)
 
 """
 説明変数のみを格納したDataFrameの作成
 """
 express = Matrix(tlm[7:end])
 pred = tsne(express)
+# pred = tsne(express, 3)
 
-# scatter(pred[:,1], pred[:,2])
-
-function t_sne_scat()
-    scatter(pred[findall(==(unique(numerized_mc)[1]), tlm[!, 4]), 1], pred[findall(==(unique(numerized_mc)[1]), tlm[!, 4]),2],
+function tsne_scat2()
+    scatter(pred[findall(==(unique(numerized_mc)[1]), tlm[!, 4]), 1], 
+                pred[findall(==(unique(numerized_mc)[1]), tlm[!, 4]),2],
                 markeralpha = 0.5,
                 markerstrokealpha = 0.,
                 )
@@ -42,5 +55,19 @@ function t_sne_scat()
                 markeralpha = 0.5,
                 markerstrokealpha = 0.,) for id in 2:9][end]
 end
-t_sne_scat()
+function tsne_scat3()
+    scatter(pred[findall(==(unique(numerized_mc)[1]), tlm[!, 4]), 1], 
+                pred[findall(==(unique(numerized_mc)[1]), tlm[!, 4]),2],
+                pred[findall(==(unique(numerized_mc)[1]), tlm[!, 4]),3],
+                markeralpha = 0.5,
+                markerstrokealpha = 0.,
+                )
+    [scatter!(pred[findall(==(unique(numerized_mc)[id]), tlm[!, 4]), 1],
+                pred[findall(==(unique(numerized_mc)[id]), tlm[!, 4]),2],
+                pred[findall(==(unique(numerized_mc)[id]), tlm[!, 4]),3],
+                markeralpha = 0.5,
+                markerstrokealpha = 0.,) for id in 2:9][end]
+end
+tsne_scat2()
+# tsne_scat3()
 
